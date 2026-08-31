fd8d9965267b4a77cc503428d045dba432a22b1f
Mahboob Chowdhury <pherkad.salm.kraz@mercor.expert>
2026-08-31 02:18:25 -0500

    Update for CORS Origin.

--- src/main/java/com/engineeringbench/AskController.java
+++ src/main/java/com/engineeringbench/AskController.java
@@ -16,7 +16,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
 public class AskController {
 
     private final SemanticSearchService searchService;
