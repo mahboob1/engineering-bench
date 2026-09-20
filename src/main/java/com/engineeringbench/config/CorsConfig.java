207fcb3b2194db603d248cc46a71c379fe6371ed
Mahboob Chowdhury <pherkad.salm.kraz@mercor.expert>
2026-09-19 23:15:05 -0500

    update for multiple cors allowed origin for vercel and ionos aieng at bengals.com

--- src/main/java/com/engineeringbench/config/CorsConfig.java
+++ src/main/java/com/engineeringbench/config/CorsConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -15,7 +17,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigin)
+                .allowedOrigins( Arrays.stream(allowedOrigin.split(","))
+                        .map(String::trim)
+                        .toArray(String[]::new))
                 .allowedMethods(
                         "GET",
                         "POST",
